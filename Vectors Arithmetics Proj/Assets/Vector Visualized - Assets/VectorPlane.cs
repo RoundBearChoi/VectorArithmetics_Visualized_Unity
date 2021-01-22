444aaa8c90f582cd48bb5cc9a3405716f2327707
RoundBearChoi <roundbeargames@gmail.com>
2021-01-23 00:10:16 +0900

    adjustable plane

--- Vectors Arithmetics Proj/Assets/Vector Visualized - Assets/VectorPlane.cs
+++ Vectors Arithmetics Proj/Assets/Vector Visualized - Assets/VectorPlane.cs
@@ -6,16 +6,30 @@ namespace Roundbeargames
 {
     public class VectorPlane : MonoBehaviour
     {
-        [SerializeField] GameObject VectorLinePrefab = null;
+        [SerializeField] GameObject VisualizedVectorPrefab = null;
         [SerializeField] GameObject MousePositionPrefab = null;
         [SerializeField] GameObject VectorMoverPrefab = null;
+        [SerializeField] GameObject PlanePrefab = null;
+        [SerializeField] GameObject BackgroundLinesPrefab = null;
         [SerializeField] Color color = new Color();
+        [SerializeField] Vector3 PlanePosition = new Vector3();
+        [SerializeField] Vector3 PlaneScale = new Vector3();
+
+        GameObject plane = null;
 
         private void Start()
         {
-            Instantiate(VectorLinePrefab, this.transform);
+            Instantiate(VisualizedVectorPrefab, this.transform);
             Instantiate(MousePositionPrefab, this.transform);
             Instantiate(VectorMoverPrefab, this.transform);
+            Instantiate(BackgroundLinesPrefab, this.transform);
+            plane = Instantiate(PlanePrefab, PlanePosition + this.transform.position, Quaternion.Euler(-90f, 0f, 0f), this.transform);
+        }
+
+        private void LateUpdate()
+        {
+            plane.transform.localPosition = PlanePosition;
+            plane.transform.localScale = PlaneScale;
         }
 
         public Color GetColor()
