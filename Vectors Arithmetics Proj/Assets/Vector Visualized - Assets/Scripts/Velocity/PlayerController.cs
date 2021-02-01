0ab0a3678fba263bc45e0f30f41603eea36d7d71
RoundBearChoi <roundbeargames@gmail.com>
2021-02-01 16:30:16 +0900

    adjustable velocity, acceleration

--- Vectors Arithmetics Proj/Assets/Vector Visualized - Assets/Scripts/Velocity/PlayerController.cs
+++ Vectors Arithmetics Proj/Assets/Vector Visualized - Assets/Scripts/Velocity/PlayerController.cs
@@ -1,57 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Roundbeargames
 {
     public class PlayerController : MonoBehaviour
     {
+        float MaxVelocity = 0f;
+        float MaxAcceleration = 0f;
+        float AccelerationMagnitude = 0f;
+        float MaxPosition = 0f;
+        float SlowDownSpeed = 0f;
+
         [Header("Setup")]
-        [SerializeField] float MaxVelocity = 0f;
-        [SerializeField] float MaxAcceleration = 0f;
-        [SerializeField] float AccelerationMagnitude = 0f;
-        [SerializeField] float MaxPosition = 0f;
-        [SerializeField] float SlowDownSpeed = 0f;
+        [SerializeField] Slider MaxVelocitySlider = null;
+        [SerializeField] Slider MaxAccelerationSlider = null;
+        [SerializeField] Slider AccelerationMagnitudeSlider = null;
+        [SerializeField] Slider MaxPositionSlider = null;
+        [SerializeField] Slider SlowDownSpeedSlider = null;
 
         [Header("Debug")]
         [SerializeField] Vector3 Velocity = new Vector3();
         [SerializeField] Vector3 Acceleration = new Vector3();
         [SerializeField] Vector3 PlayerPosition = new Vector3();
 
-        bool keyIsPressed = false;
-
         private void Update()
         {
+            UpdateStats();
+            MovePlayer();
+        }
+
+        void UpdateStats()
+        {
+            MaxVelocity = MaxVelocitySlider.value;
+            MaxAcceleration = MaxAccelerationSlider.value;
+            AccelerationMagnitude = AccelerationMagnitudeSlider.value;
+            MaxPosition = MaxPositionSlider.value;
+            SlowDownSpeed = SlowDownSpeedSlider.value;
+        }
+
+        void MovePlayer()
+        {
             Acceleration = Vector3.zero;
-            keyIsPressed = false;
 
             if (Input.GetKey(KeyCode.W))
             {
                 Acceleration += new Vector3(0f, AccelerationMagnitude, 0f);
-                keyIsPressed = true;
             }
 
             if (Input.GetKey(KeyCode.A))
             {
                 Acceleration -= new Vector3(AccelerationMagnitude, 0f, 0f);
-                keyIsPressed = true;
             }
 
             if (Input.GetKey(KeyCode.S))
             {
                 Acceleration -= new Vector3(0f, AccelerationMagnitude, 0f);
-                keyIsPressed = true;
             }
 
             if (Input.GetKey(KeyCode.D))
             {
                 Acceleration += new Vector3(AccelerationMagnitude, 0f, 0f);
-                keyIsPressed = true;
             }
 
             ClampVector(ref Acceleration, MaxAcceleration);
             Velocity += (Acceleration * Time.deltaTime);
-            
+
             ClampVector(ref Velocity, MaxVelocity);
             PlayerPosition += (Velocity * Time.deltaTime);
 
